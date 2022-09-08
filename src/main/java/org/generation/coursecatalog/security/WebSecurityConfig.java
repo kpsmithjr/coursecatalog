a38428783011d401c8729900c4f6cdf128d7412e
Ken Smith <kpsc59@umstem.edu>
2022-09-08 15:59:29 -0500

    Authentication

--- src/main/java/org/generation/coursecatalog/security/WebSecurityConfig.java
+++ src/main/java/org/generation/coursecatalog/security/WebSecurityConfig.java
@@ -1,12 +1,16 @@
 package org.generation.coursecatalog.security;
 
+import org.generation.coursecatalog.filter.CustomAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -27,6 +31,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     http.csrf().disable();
     // http.authorizeHttpRequests().antMatchers("*").permitAll();
     http.authorizeHttpRequests().anyRequest().permitAll();
+    http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
   }
   
+  @Bean
+  public AuthenticationManager AuthenticationManagerBean() throws Exception {
+    return super.authenticationManager();
+  }
 }
